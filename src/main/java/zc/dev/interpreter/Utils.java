cf55ed4967195ae8cc086ca59f10f53d454a91c0
zilvinas <zilvinas.ciuprinskas@ruptela.com>
2025-01-31 14:55:24 +0200

    ..

--- src/main/java/zc/dev/interpreter/Utils.java
+++ src/main/java/zc/dev/interpreter/Utils.java
@@ -8,6 +8,12 @@ public class Utils {
     }
 
     public static String getCode() {
+        String filePath = getFilePath();
+        TextFileReader reader = TextFileReader.of(filePath);
+        return String.join("\n", reader.readAll());
+    }
+
+    private static String getFilePath() {
         File currentDir = new File(".");
         String dir0 = currentDir.getAbsolutePath();
         boolean isWindows = dir0.contains(":\\\\");
@@ -15,8 +21,6 @@ public class Utils {
                 ?"\\src\\main\\java\\zc\\dev\\interpreter\\test_code\\"
                 :"/src/main/java/zc/dev/interpreter/test_code/";
         String fileName = "TestCode.java";
-        String filePath = dir0 + dir1 + fileName;
-        TextFileReader reader = TextFileReader.of(filePath);
-        return String.join("\n", reader.readAll());
+        return dir0 + dir1 + fileName;
     }
 }
